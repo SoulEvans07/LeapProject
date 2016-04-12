5a76d4b8418202af0dd9a826a40b553c8b0e8a13
SoulEvans07@github.com <SoulEvans07@github.com>
2016-04-12 17:00:14 +0200

    working bluetooth connection and stream. won't write "Connected" if there is no bluetooth enabled.

--- src/bluetooth/Main.java
+++ src/bluetooth/Main.java
@@ -2,6 +2,7 @@ package bluetooth;
 
 public class Main {
     public static void main(String[] args){
+        // TODO: if CRLF/LF error write in terminal: "git config core.autocrlf false"
         KeyFunc window = new KeyFunc();
     }
 
